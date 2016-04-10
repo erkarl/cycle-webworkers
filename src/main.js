6757ca566ed9be5f99c2cc2a5f37ea54126f0dce
Karl Ranna <karl@karlranna.com>
2016-04-10 19:48:14 +0300

    Create basic webworker driver

--- src/main.js
+++ src/main.js
@@ -1,21 +1,29 @@
 import Rx from 'rx';
 import Cycle from '@cycle/core';
 import {makeDOMDriver, div, input, p} from '@cycle/DOM';
-import webworker from './webworker.js';
+import makeWebworkerDriver from './webworker.js';
 
-function main(drivers) {
+function main({DOM, webWorkers}) {
+  const incomingWebworkers$ = webWorkers;
+  webWorkers.subscribe(function(msg){
+    console.log('incoming webworker', msg);
+  });
+  let outgoingWebworkers$ = Rx.Observable.interval(5000)
+    .map(val => 'loop.js')
   return {
     DOM: Rx.Observable.of('')
       .map(someVal =>
         div([
           p('Cycle WebWorkers')
         ])
-      )
+      ),
+    webWorkers: outgoingWebworkers$
   };
 }
 
 const drivers = {
-  DOM: makeDOMDriver('#app')
+  DOM: makeDOMDriver('#app'),
+  webWorkers: makeWebworkerDriver('loop.js')
 };
 
 Cycle.run(main, drivers);
--- src/webworker.js
+++ src/webworker.js
@@ -1,8 +1,19 @@
-if (window.Worker) {
-  console.log('webWorker');
-  let webWorker = new Worker('loop.js')
-  webWorker.onmessage = function(e) {
-    console.log('For loop done outside worker.');
-  };
-  webWorker.postMessage([]);
-}
+function makeWebworkerDriver(fileName) {
+  console.log('new webworker', fileName);
+  let worker = new Worker('loop.js');
+  return function webWorkerDriver(outgoing$) {
+    let incoming$ = Rx.Observable.create(function(observer){
+      worker.onmessage = msg => {
+        console.log('incoming msg', msg);
+        observer.onNext(msg);
+      }
+    });
+    outgoing$.subscribe(function(outgoing){
+      console.log('worker outgoing');
+      worker.postMessage(outgoing)
+    });
+    return incoming$
+  }
+};
+
+module.exports = makeWebworkerDriver;
