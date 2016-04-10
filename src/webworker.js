c82f07bbff032f81ad90ece4f84c0ac6d40c42a1
Karl Ranna <karl@karlranna.com>
2016-04-10 20:50:12 +0300

    Use let instead of var for defining a worker

--- src/webworker.js
+++ src/webworker.js
@@ -1,5 +1,5 @@
 function makeWebworkerDriver(fileName) {
-  var worker = new Worker('loop.js');
+  let worker = new Worker(fileName);
   return function webWorkerDriver(outgoing$) {
     let incoming$ = Rx.Observable.create(function(observer){
       worker.onmessage = msg => {
